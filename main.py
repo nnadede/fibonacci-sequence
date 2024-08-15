7b4ef7c6b5f1c845c1d9f80b554e410d7866bbe4
Nnaemeka Nnadede <nnadede@cs.dal.ca>
2024-08-15 01:33:46 -0300

    returns each value to be added or printed

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 #   Program to print fibonacci numbers from n = 1 to the given nth term
 n_1_term = 0
 n_2_term = 1
-count = 0
+count = 1
 sum = lambda a, b: a + b
 
-def fib(nth_term):
-    if (count == 0):
+def fib(nth_term, count, n_1_term, n_2_term):
+    if (count == 1):
         num = n_1_term
-    elif (count == 1):
+    elif (count == 2):
         num = n_2_term
     else:
         num = sum(n_1_term, n_2_term)
         n_1_term = n_2_term
         n_2_term = num
     if (count != nth_term):
-        return fib(nth_term)
+        count+=1
+        return fib(nth_term, count, n_1_term, n_2_term)
     else:
         return num
 
@@ -22,15 +23,14 @@ def fib(nth_term):
 print("Displaying fibonacci numbers from the 1st term to the nth term\n\
     Taking the 1st term as 0.")
 
-nth_term = int(input("Enter nth term: "))
-
 try:
+    nth_term = int(input("Enter nth term: "))
     if (nth_term > 0):
-        print(fib(nth_term))
+        print(fib(nth_term, count, n_1_term, n_2_term))
     else:
-        raise ValueError("Enter nth term greater than 0") 
-except:
-    print("Invalid Value")
+        print("Enter nth term greater than 0") 
+except ValueError as e:
+    print(f'Invalid Value {e}')
 
 
 
